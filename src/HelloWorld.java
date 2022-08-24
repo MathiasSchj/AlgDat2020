dfe700e74a9c12bdba9d060f6d52a45a10eb68cb
GreekWong <Mathias.schjetne@gmail.com>
2022-08-24 12:38:02 +0200

    Første push

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -2,7 +2,15 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello World");
 
+        System.out.println("teller til 10");
+
         for (int i = 1; i <= 10; i++){
+            System.out.println(i);
+        }
+
+        System.out.println("Petra teller til 20");
+
+        for (int i = 1; i <= 20; i++){
             System.out.println(i);
         }
     }
